14eb71cd775b93410955d4f67c273bdaa78d8825
saicharan416 <58965539+saicharan416@users.noreply.github.com>
2019-12-20 10:13:15 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    char b='a';
+    float c=12.89;
+    printf("\nsizeof(a)=%d",sizeof(a));
+    printf("\nsizeof(b)=%d",sizeof(b));
+    printf("\nsizeof(c)=%d",sizeof(c));
+    printf("\nsizeof(int)=%d",sizeof(int));
+    printf("\nsizeof(char)=%d",sizeof(char));
+    printf("\nsizeof(float)=%d",sizeof(float));
+    return 0;
+}
